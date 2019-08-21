622ae67bbf05879112d0effd6659838ee2832124
joher <skjoher@protonmail.com>
2019-08-21 15:33:52 +0530

    add JVM monitoring

---
+++ jmxtrans/setenv.sh
@@ -0,0 +1,1 @@
+export CATALINA_OPTS="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9999 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=10.10.87.244"
